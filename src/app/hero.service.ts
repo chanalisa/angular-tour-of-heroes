6360ababec05859f229eeeeb4f6962e04e2b1d14
Alisa Chan <alisachan.ny@gmail.com>
2020-02-21 16:26:29 -0500

    added HeroService and MessageService

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+import { MessageService } from "./message.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class HeroService {
+  constructor(public messageService: MessageService) {}
+
+  // of(HEROES) returns an Observable<Hero[]> that emits a single value (the array of mock heroes)
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add("HeroService: Fetched heroes");
+    return of(HEROES);
+  }
+}
